a0a4860824a1a64a04a67fd8debc400b45984b83
Office\Yu <atwind@cszi.com>
2018-06-25 09:27:49 +0800

    rename the project BeesTask

---
+++ src/RS.BeesTask.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RS.BeeTask.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
